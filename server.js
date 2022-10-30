3635fa0d7fa3b3c8f10ce3fa9ef1b59db89668b1
Liam Hutchinson <liamhutch1000@gmail.com>
2022-10-30 18:28:51 -0400

    assignment 3

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+* BTI325 – Assignment 2
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Liam Hutchinson Student ID: 184017218 Date: 10/09/2022
+*
+* Online (Cyclic) URL:
+* https://ill-puce-abalone-gear.cyclic.app/
+*
+********************************************************************************/ 
 var express = require("express");
 var app = express();
 var path = require("path");
